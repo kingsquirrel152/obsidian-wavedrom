4ae6dfd164be5ef548de94d750050f5376969fc4
Alex Stewart <astewart@starbucks.com>
2021-08-05 19:34:47 -0400

    Capture patches
    - Now with per graph rendering goodness.

--- main.ts
+++ main.ts
@@ -11,26 +11,30 @@ export default class ObsidianWaveDrom extends Plugin {
 
 	wavedromProcessor = async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
 		
+		// Give our wavedrom input node a unique name
+		let r = (Math.random() + 1).toString(36).substring(7);
+
 		// Create the source object
 		const wrapper = document.createElement('div');
 		wrapper.setAttribute('align', 'center');
 
-		const dest = document.createElement('script');
-		dest.setAttribute('type', 'WaveDrom');
-		source = source.replace(/\n/g, " ");
-        dest.innerText = source;
-
-		wrapper.appendChild(dest);
+		// Use the parent wrapper as the wavedrom display
+		wrapper.setAttribute('id', 'WaveDrom_Display_' + r);
 		el.appendChild(wrapper);
 
-		// Probably a better way to do this
-		// This works for now though.
-		setTimeout(() => {	  
-     		WaveDrom.ProcessAll();
-		}, 5);
 
-		//console.log('Processed');
+		// Create script node with wavedrom json contents
+		const inputjson = document.createElement('script');
+		inputjson.setAttribute('type', 'WaveDrom');
+		source = source.replace(/\n/g, " ");
+        inputjson.innerText = source;
+		inputjson.setAttribute('id', 'InputJSON_' + r);
 
+		// Eval the script node into a form the wavedrom expects
+		var obj = WaveDrom.evaObject(inputjson);
+
+		// Render it to the wrapper div!
+		WaveDrom.RenderWaveElement(r, obj, wrapper, window.WaveSkin, false);
     };
 
     onload(): void {
@@ -45,6 +49,9 @@ export default class ObsidianWaveDrom extends Plugin {
 	
 			document.head.appendChild(wavedromDefault);
 			document.head.appendChild(wavedromMin);
+
+			document.head.innerHTML += '<style type="text/css">div.wavedromMenu{position:fixed;border:solid 1pt#CCCCCC;background-color:white;box-shadow:0px 10px 20px #808080;cursor:default;margin:0px;padding:0px;}div.wavedromMenu>ul{margin:0px;padding:0px;}div.wavedromMenu>ul>li{padding:2px 10px;list-style:none;}div.wavedromMenu>ul>li:hover{background-color:#b5d5ff;}</style>';
+
 		}
 
         this.registerMarkdownCodeBlockProcessor("wavedrom", this.wavedromProcessor);
